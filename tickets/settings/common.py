e57b6b95e4f412abded17319a4875e4c5addb587
Greg Drake <greg@madmallards.com>
2026-09-23 20:42:27 -0400

    Allow configurable SQLite database path

--- tickets/settings/common.py
+++ tickets/settings/common.py
@@ -99,7 +99,7 @@ WSGI_APPLICATION = 'tickets.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.getenv('DJANGO_DB_PATH', os.path.join(BASE_DIR, 'db.sqlite3')),
     }
 }
 
