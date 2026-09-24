bc14b199ed268c4cb0c48380a42f9e3c91806027
Greg Drake <greg@madmallards.com>
2026-09-23 20:27:41 -0400

    Test shared dashboard template rendering

--- dashboard/tests.py
+++ dashboard/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class DashboardTemplateTests(TestCase):
+    def test_dashboard_renders_shared_layout(self):
+        response = self.client.get(reverse("dashboard:index"))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "ServDesk")
+        self.assertContains(response, 'data-bs-toggle="dropdown"')
