99c4ad3ad27902b30d21b89a04998546f6024122
Greg Drake <greg@madmallards.com>
2026-09-23 22:07:36 -0400

    Add knowledge base create and detail routes

--- knowledgebase/urls.py
+++ knowledgebase/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 
 from . import views
 
-app_name = 'knowledgebase'
+app_name = "knowledgebase"
+
 urlpatterns = [
-    path('', views.index, name='index'),
+    path("", views.index, name="index"),
+    path("new/", views.article_new, name="new"),
+    path("<int:article_id>/", views.article_detail, name="detail"),
 ]
