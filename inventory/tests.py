936097f544409cb2c4e8e1d92e1f994de4c51409
Greg Drake <greg@madmallards.com>
2026-09-23 21:41:30 -0400

    Test inventory write authorization

--- inventory/tests.py
+++ inventory/tests.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
 from django.utils import timezone
@@ -7,6 +8,15 @@ from .models import Cluster, Operating_System, Server, Vendor
 
 class InventoryWorkflowTests(TestCase):
     def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username="inventory-user",
+            password="test-password",
+        )
+        self.staff_user = get_user_model().objects.create_user(
+            username="inventory-admin",
+            password="test-password",
+            is_staff=True,
+        )
         self.os = Operating_System.objects.create(os_name="Ubuntu")
         self.vendor = Vendor.objects.create(vendor_name="Dell")
         self.cluster = Cluster.objects.create(cluster_name="Production")
@@ -53,7 +63,15 @@ class InventoryWorkflowTests(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, str(self.os.pk))
 
+    def test_server_create_requires_authentication(self):
+        response = self.client.get(reverse("inventory:servers_new"))
+
+        self.assertEqual(response.status_code, 302)
+        self.assertIn(reverse("login"), response["Location"])
+
     def test_server_form_creates_server(self):
+        self.client.force_login(self.user)
+
         response = self.client.post(
             reverse("inventory:servers_new"),
             {
@@ -67,3 +85,27 @@ class InventoryWorkflowTests(TestCase):
         self.assertEqual(response.status_code, 200)
         created = Server.objects.get(server_name="app-02")
         self.assertEqual(created.cluster, self.cluster)
+
+    def test_vendor_create_requires_authentication(self):
+        response = self.client.get(reverse("inventory:vendor_new"))
+
+        self.assertEqual(response.status_code, 302)
+        self.assertIn(reverse("login"), response["Location"])
+
+    def test_vendor_create_rejects_non_staff_user(self):
+        self.client.force_login(self.user)
+
+        response = self.client.get(reverse("inventory:vendor_new"))
+
+        self.assertEqual(response.status_code, 403)
+
+    def test_staff_user_can_create_vendor(self):
+        self.client.force_login(self.staff_user)
+
+        response = self.client.post(
+            reverse("inventory:vendor_new"),
+            {"vendor_name": "Lenovo"},
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(Vendor.objects.filter(vendor_name="Lenovo").exists())
