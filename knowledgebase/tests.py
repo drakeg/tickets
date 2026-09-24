10b336352dfc90300b5a1e7cc188c1294dfa7ea4
Greg Drake <greg@madmallards.com>
2026-09-23 20:43:57 -0400

    Add knowledge base listing test

--- knowledgebase/tests.py
+++ knowledgebase/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.utils import timezone
 
-# Create your tests here.
+from .models import Article
+
+
+class KnowledgeBaseTests(TestCase):
+    def test_index_lists_articles(self):
+        article = Article.objects.create(
+            author="Support",
+            description="Reset a locked account",
+            keywords="account,password",
+            pub_date=timezone.now(),
+        )
+
+        response = self.client.get(reverse("knowledgebase:index"))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(list(response.context["knowledgebase_list"]), [article])
