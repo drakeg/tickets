050d99b332bbb10815d5071decfa3410c3a56637
Greg Drake <greg@madmallards.com>
2026-09-23 21:50:46 -0400

    Include project summary in creation form

--- projects/forms.py
+++ projects/forms.py
@@ -4,4 +4,4 @@ from .models import Project
 class ProjectForm(forms.ModelForm):
 	class Meta:
 		model = Project
-		fields = ('description', 'start_date', 'end_date', 'assigned',)
+		fields = ('summary', 'description', 'start_date', 'end_date', 'assigned',)
